d99fb45b32d1cf11157f5986a026fb689c0e3440
Brian Kaloki <brianngenye@gmail.com>
2025-05-12 11:54:24 +0300

    my first commit

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,12 @@
+# car.py
+
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, wheel_size, wheel_number, model="Generic Model"):
+        # Initialize the parent class with the wheel_size and wheel_number attributes
+        super().__init__(wheel_size, wheel_number)
+        self.model = model  # Specific attribute for the Car class
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"  # Overridden go method for the car
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,12 @@
+# vehicle.py
+
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "vrrrrrrrooom!"  # Default go method for a generic vehicle
+
+    def fill_up_tank(self):
+        return "filling up!"  # Method to simulate refueling the vehicle
